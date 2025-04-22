3ef64509745300b81e3660795ae877426f542d82
lily4499 <77320224+lily4499@users.noreply.github.com>
2025-04-22 15:19:36 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
 
+
+mongoose.connect('mongodb://localhost:27017/myapp');
 
 app.get('/', (req,res) => {
   res.sendFile(path.join(__dirname+'/index.html'));
